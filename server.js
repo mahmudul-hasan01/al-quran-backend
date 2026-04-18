2a4c0f22acca62d5a5d10156a55c2c543eee0c4a
mahmudul-hasan01 <mahamudul2018@gmail.com>
2026-04-18 16:25:08 +0600

    Add .

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import quranRoutes from "./routes/quranRoutes.js";
 
 dotenv.config();
 
@@ -10,11 +11,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// use routes
+app.use("/api", quranRoutes);
+
 // MongoDB connect
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch((err) => console.log("❌ MongoDB error:", err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("MongoDB error:", err));
 
 app.get("/", (req, res) => {
   res.send("Quran API running...");
@@ -25,6 +29,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
-
-// MONGO_URI=mongodb+srv://quran-api-backend:3DAZ8C7mFvGcQ8DV@cluster0.uoehazd.mongodb.net/?appName=Cluster0
-// PORT=5000
