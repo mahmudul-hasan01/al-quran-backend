27b10df12e9eae639177d921d39a6b6f7000974c
mahmudul-hasan01 <mahamudul2018@gmail.com>
2026-04-19 03:57:43 +0600

    Solve cors issue.

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://al-quran-snowy.vercel.app",
+  }),
+);
 app.use(express.json());
 
 // use routes
