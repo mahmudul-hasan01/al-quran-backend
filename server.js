87a876109b3d0099adb5226aa8f0e3ff6b759fbd
mahmudul-hasan01 <mahamudul2018@gmail.com>
2026-05-09 02:35:04 +0600

    Change mongoose connection.

--- server.js
+++ server.js
@@ -8,21 +8,17 @@ dotenv.config();
 
 const app = express();
 
+// Middleware
 app.use(
   cors({
     origin: ["https://al-quran-snowy.vercel.app", "http://localhost:3000"],
   }),
 );
+
 app.use(express.json());
 
-// use routes
+// Routes
 app.use("/api", quranRoutes);
-
-// MongoDB connect
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log("MongoDB error:", err));
 
 app.get("/", (req, res) => {
   res.send("Quran API running...");
@@ -30,6 +26,19 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+// MongoDB Connection
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("✅ MongoDB Connected");
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log("❌ MongoDB connection error:", error);
+  }
+};
+
+startServer();
